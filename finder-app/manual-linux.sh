79ee57436ee220528fe7b82e3cde842df8046e44
y-ozkaya <yasirozkaya1@gmail.com>
2025-01-05 18:13:40 +0100

    finder-test.sh modified

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -51,7 +51,7 @@ cd "$OUTDIR"
 if [ -d "${OUTDIR}/rootfs" ]
 then
 	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
-    sudo rm  -rf ${OUTDIR}/rootfs
+    sudo rm -rf ${OUTDIR}/rootfs
 fi
 
 # TODO: Create necessary base directories
